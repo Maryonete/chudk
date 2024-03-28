c7d030df7adbdab33e93d45a4c63585c8371e2d8
Maryonete <mauricemarion@hotmail.com>
2024-03-28 17:50:39 +0100

    restore environment variable

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = "59c66f5ba1d72c42ad99045be7dc959b"
 # appel a la BD une seule fois par jour ! 
-GET_DATA_FROM_FILE = True
-#GET_DATA_FROM_FILE = False
+#GET_DATA_FROM_FILE = True
+GET_DATA_FROM_FILE = False
--- src/utils/auth.py
+++ src/utils/auth.py
@@ -1,6 +1,6 @@
 
-#is_logged_in = False
-is_logged_in = True
+is_logged_in = False
+#is_logged_in = True
 
 def set_logged_in(value):
     global is_logged_in
